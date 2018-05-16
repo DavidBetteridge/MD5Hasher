a1ed5249131e710ca8b1e79d23fd4146134ffc9b
David Betteridge <DavidaBetteridge@gmail.com>
2018-05-16 22:17:50 +0100

    New

---
+++ Hasher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MD5Hasher
+{
+    public class Hasher
+    {
+        public string HashedPassword(string password)
+        {
+            var hash = "";
+            using (var md5 = MD5.Create())
+            {
+                var hashedBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+                hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+
+            return hash;
+        }
+
+    }
+}
